739ed8753af2dcc7218e773cce0d20f586f61311
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-04-22 00:39:01 +0300

    feat: add transaction provider with CRUD and SharedPreferences

--- lib/features/transaction/providers/transaction_provider.dart
+++ lib/features/transaction/providers/transaction_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocketwise/core/models/transaction_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 class TransactionProvider extends Notifier<List<TransactionModel>>{
   @override
@@ -19,12 +20,13 @@ class TransactionProvider extends Notifier<List<TransactionModel>>{
     final String? savedData = _prefs.getString("transactions");
     if (savedData != null) {
       final List<dynamic> decodedData = jsonDecode(savedData);
-      state = decodedData.map((element) => TransactionModel.fromJson((element))).toList();
+      state = decodedData.reversed.map((element) => TransactionModel.fromJson((element))).toList();
     }
   }
 
-  void addTransaction(TransactionModel transaction) async {
-    state = [... state, transaction];
+  void addTransaction(String title, double amount, String category, DateTime createdAt, TransactionType type) async {
+    TransactionModel transactionModel = TransactionModel(id: const Uuid().v4(), title: title, amount: amount, category: category, createdAt: createdAt, type: type);
+    state = [transactionModel, ... state];
     final String encodedData = jsonEncode(state.map((transaction) => transaction.toJson()).toList());
     await _prefs.setString("transactions", encodedData);
   }
@@ -39,6 +41,24 @@ class TransactionProvider extends Notifier<List<TransactionModel>>{
     state = [];
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove("transactions");
+  }
+
+  double get totalIncome {
+    return state.where((t) => t.type == TransactionType.income).fold(
+      0.0,
+      (sum, t) => sum+t.amount
+    );
+  }
+
+  double get totalExpense {
+    return state.where((t) => t.type == TransactionType.expense).fold(
+      0.0,
+      (sum, t) => sum+t.amount
+    );
+  }
+
+  double get totalBalance {
+    return totalIncome - totalExpense;
   }
 
 }
