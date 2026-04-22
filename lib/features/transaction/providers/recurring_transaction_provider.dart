9beba3266bc9d019fce20732c5b62578853aae76
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-04-23 00:00:14 +0300

    feat: add recurring transaction provider with auto-processing

---
+++ lib/features/transaction/providers/recurring_transaction_provider.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocketwise/core/models/recurring_transaction_model.dart';
+import 'package:pocketwise/core/models/transaction_model.dart';
+import 'package:pocketwise/features/transaction/providers/transaction_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+
+class RecurringTransactionProvider extends Notifier<List<RecurringTransactionModel>> {
+  @override
+  List<RecurringTransactionModel> build() {
+    // TODO: implement build
+    _loadRecurringTransactions();
+    return [];
+  }
+
+  late SharedPreferences _prefs;
+
+  void _processRecurringTransactions() async {
+    for (var recurring in state) {
+      int periodsElapsed = 0;
+      final daysDiff = DateTime.now().difference(recurring.lastProcessDate).inDays;
+      if (recurring.isActive) {
+        if (recurring.endDate != null && DateTime.now().isAfter(recurring.endDate!)) {
+          continue;
+        }
+        switch (recurring.frequency) {
+          case RecurringFrequency.daily:
+            periodsElapsed = daysDiff;
+          break;
+          case RecurringFrequency.weekly:
+            periodsElapsed = daysDiff~/7;
+          break;
+          case RecurringFrequency.monthly:
+            periodsElapsed = daysDiff~/30;
+          break;
+          case RecurringFrequency.yearly:
+            periodsElapsed = daysDiff~/365;
+          break;
+        }
+        if (periodsElapsed > 0) {
+          for (int i=0; i<periodsElapsed; i++) {
+            ref.read(transactionProvider.notifier).addTransaction(
+              recurring.title,
+              recurring.amount,
+              recurring.category,
+              recurring.lastProcessDate.add(Duration(days: i+1)),
+              recurring.type
+            );
+          }
+        }
+        state = state.map((element) {
+          if (element.id == recurring.id) {
+            return element.copyWith(lastProcessDate: DateTime.now());
+          }
+          return element;
+        }).toList();
+      }
+    }
+    _prefs = await SharedPreferences.getInstance();
+    final String encodedData = jsonEncode(state.map((r) => r.toJson()).toList());
+    await _prefs.setString("recurring_transactions", encodedData);
+  }
+
+  void _loadRecurringTransactions() async {
+    _prefs = await SharedPreferences.getInstance();
+    final String? savedData = _prefs.getString("recurring_transactions");
+    if (savedData != null) {
+      final List<dynamic> decodedData = jsonDecode(savedData);
+      state = decodedData.map((element) => RecurringTransactionModel.fromJson((element))).toList();
+      state.sort((a, b) => b.startDate.compareTo(a.startDate));
+    }
+    _processRecurringTransactions();
+  }
+
+  void addRecurring({
+    required String title,
+    required double amount,
+    required String category,
+    required DateTime startDate,
+    required RecurringFrequency frequency,
+    required TransactionType type,
+    required bool isActive,
+    DateTime? endDate,
+  }) async {
+    RecurringTransactionModel recurringTransactionModel = RecurringTransactionModel(
+      id: const Uuid().v4(),
+      title: title,
+      amount: amount,
+      category: category,
+      lastProcessDate: startDate,
+      startDate: startDate,
+      endDate: endDate,
+      frequency: frequency,
+      type: type,
+      isActive: isActive
+    );
+    state = [recurringTransactionModel, ...state];
+    state.sort((a, b) => b.startDate.compareTo(a.startDate));
+    final String encodedData = jsonEncode(state.map((recurringTransaction) => recurringTransaction.toJson()).toList());
+    await _prefs.setString("recurring_transactions", encodedData);
+  }
+
+  void removeRecurring(String id) async {
+    state = state.where((element) => element.id != id).toList();
+    final String encodedData = jsonEncode(state.map((recurringTransaction) => recurringTransaction.toJson()).toList());
+    await _prefs.setString("recurring_transactions", encodedData);
+  }
+
+  void toggleIsActive(String id) async {
+    state = state.map((element) {
+      if (element.id == id) {
+        return element.copyWith(isActive: !element.isActive);
+      }
+      return element;
+    }).toList();
+    state.sort((a, b) => b.startDate.compareTo(a.startDate));
+    final String encodedData = jsonEncode(state.map((recurringTransaction) => recurringTransaction.toJson()).toList());
+    await _prefs.setString("recurring_transactions", encodedData);
+  }
+
+}
+
+final recurringTransactionProvider = NotifierProvider<RecurringTransactionProvider, List<RecurringTransactionModel>>(RecurringTransactionProvider.new);
