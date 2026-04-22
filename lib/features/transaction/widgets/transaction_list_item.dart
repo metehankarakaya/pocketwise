c23c25f7c4adb23f21437fb8afc59bccb53a3785
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-04-23 00:01:35 +0300

    fix: smart date formatting

--- lib/features/transaction/widgets/transaction_list_item.dart
+++ lib/features/transaction/widgets/transaction_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:pocketwise/core/constants/app_strings.dart';
 import 'package:pocketwise/core/models/transaction_model.dart';
 
